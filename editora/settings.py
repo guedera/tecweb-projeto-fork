e3cb6021f67815f97ee835c8bda7421853d3a171
guedera <guilhermegg5@al.insper.edu.br>
2025-03-13 09:08:56 -0300

    requirements

--- editora/settings.py
+++ editora/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,11 @@ WSGI_APPLICATION = 'editora.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://exemplo_livro_l25z_user:DmAB8pT6x899b9A7eEmIohZymqCt1ZKc@dpg-cv9c73a3esus73e1hnlg-a.oregon-postgres.render.com/exemplo_livro_l25z',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 
